7f30c92d20f91bd5c7bf1552d1550827935b48a9
michellelallystudent <G00351333@gmit.ie>
2019-02-12 21:29:52 +0000

    more research python re

--- project.py
+++ project.py
@@ -1,12 +1,2 @@
 # Michelle Lally G00351333
-# Calculate the fatorial of a number
-
-start = 10
-ans = 1
-i = 1
-
-while i <= start:
-    ans = ans * i
-    i = i + 1
-
-print(ans)
+# Graph Theory Project 2019
